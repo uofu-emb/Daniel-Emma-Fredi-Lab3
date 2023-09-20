23d6db906bc5e7eddae3e613cbb7c932bd618e55
dknelson9876 <dknelson9876@gmail.com>
2023-09-20 15:39:25 -0600

    Protect critical section in thread loop

--- test/threads.c
+++ test/threads.c
@@ -11,45 +11,49 @@ K_THREAD_STACK_DEFINE(coop_stack, STACKSIZE);
 
 struct k_sem semaphore;
 int counter;
-void thread_entry(void)
-{
-	struct k_timer timer;
-	k_timer_init(&timer, NULL, NULL);
+void thread_entry(void) {
+    struct k_timer timer;
+    k_timer_init(&timer, NULL, NULL);
 
-	while (1) {
-        counter = counter + 1;
-		printk("hello world from %s! Count %d\n", "thread", counter);
-		k_timer_start(&timer, K_MSEC(SLEEPTIME), K_NO_WAIT);
-		k_timer_status_sync(&timer);
-	}
+    while (1) {
+        k_sem_take(&semaphore, K_FOREVER);
+        {
+            counter = counter + 1;
+            printk("hello world from %s! Count %d\n", "thread", counter);
+            k_timer_start(&timer, K_MSEC(SLEEPTIME), K_NO_WAIT);
+            k_timer_status_sync(&timer);
+        }
+        k_sem_give(&semaphore);
+    }
 }
 
-int main(void)
-{
+int main(void) {
     counter = 0;
-    k_sem_init(&semaphore, 0, 1);
+    k_sem_init(&semaphore, 1, 1);
     k_thread_create(&coop_thread,
-                    coop_stack,
-                    STACKSIZE,
-                    (k_thread_entry_t) thread_entry,
-                    NULL,
-                    NULL,
-                    NULL,
-                    K_PRIO_COOP(7),
-                    0,
-                    K_NO_WAIT);
+        coop_stack,
+        STACKSIZE,
+        (k_thread_entry_t)thread_entry,
+        NULL,
+        NULL,
+        NULL,
+        K_PRIO_COOP(7),
+        0,
+        K_NO_WAIT);
 
-	struct k_timer timer;
-	k_timer_init(&timer, NULL, NULL);
+    struct k_timer timer;
+    k_timer_init(&timer, NULL, NULL);
 
-	while (1) {
+    while (1) {
         k_sem_take(&semaphore, K_FOREVER);
-        counter = counter + 1;
-		printk("hello world from %s! Count %d\n", "main", counter);
-		k_timer_start(&timer, K_MSEC(SLEEPTIME), K_NO_WAIT);
-		k_timer_status_sync(&timer);
+        {
+            counter = counter + 1;
+            printk("hello world from %s! Count %d\n", "main", counter);
+            k_timer_start(&timer, K_MSEC(SLEEPTIME), K_NO_WAIT);
+            k_timer_status_sync(&timer);
+        }
         k_sem_give(&semaphore);
-	}
+    }
 
-	return 0;
+    return 0;
 }
